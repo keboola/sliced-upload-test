880f8c81dfecccba63f4a043d98fe75b78b2a5e5
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2018-11-16 12:21:22 +0100

    feat: more characters

--- main.php
+++ main.php
@@ -12,7 +12,10 @@ if (isset($arguments["data"])) {
 $config = json_decode(file_get_contents($dataFolder . "/config.json"), true)["parameters"];
 
 $chars = [
-    "\t", "\n", "a", "b", "c", "d", "e", "f"
+    "\t", "\n",
+    "a", "b", "c", "d", "e", "f", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+    "A", "B", "C", "D", "E", "F", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9
 ];
 
 function generateCell($bytes, $chars)
